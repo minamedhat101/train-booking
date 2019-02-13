7d7fda9646dd3bce266b698fcee9afd56e9aa3a4
MohamedAshraf2 <44209088+MohamedAshraf2@users.noreply.github.com>
2019-02-13 18:02:10 +0200

    Add files via upload
    employess & station

---
+++ station.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const StationSchema=new mongoose.Schema({
+    
+    name:{
+        type:String,
+        required:true}    
+    });
+
+
+module.exports = mongoose.model('Station',StationSchema);
